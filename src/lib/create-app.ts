1303eb41af71f1437b9097bcf934d805bd5c0ed8
Josue Patricio <josretamozovar@gmail.com>
2025-02-03 19:18:47 -0500

    se agrego cors

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -3,6 +3,7 @@ import { OpenAPIHono } from '@hono/zod-openapi'
 import { notFound, onError, serveEmojiFavicon } from 'stoker/middlewares'
 import { defaultHook } from 'stoker/openapi'
 import pinoLogger from '../middlewares/pino-logger.js'
+import { cors } from 'hono/cors'
 
 export function createRouter(): OpenAPIHono<AppBindings> {
   return new OpenAPIHono<AppBindings>({ defaultHook, strict: false })
@@ -12,12 +13,20 @@ function createApp(): OpenAPIHono<AppBindings> {
   const app = createRouter()
   app.use(serveEmojiFavicon('🦊'))
   app.use('*', pinoLogger)
+  app.use('*', cors({
+    origin: 'http://localhost:5173',
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    exposeHeaders: ['Content-Length', 'Content-Type'],
+    maxAge: 600,
+    credentials: true,
+  }))
   app.notFound(notFound)
   app.onError(onError)
   return app
 }
 
-export function createTestApp(router:AppOpenApi){
+export function createTestApp(router: AppOpenApi) {
   const testApp = createApp()
   testApp.route('/', router)
   return testApp
